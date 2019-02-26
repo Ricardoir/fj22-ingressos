2132ed093966a658bfba490faad07edb5905d4f0
ricardoir <ricardo.iannuzzi@hotmail.com>
2019-02-25 22:33:28 -0300

    Exercicio 3.4 e 3.5

---
+++ src/main/java/br/com/caelum/ingresso/model/descontos/DescontoParaEstudantes.java
@@ -0,0 +1,11 @@
+package br.com.caelum.ingresso.model.descontos;
+import java.math.BigDecimal;
+
+public class DescontoParaEstudantes implements Desconto {
+	
+	@Override
+	public BigDecimal aplicarDescontoSobre(BigDecimal precoOrignal) {
+		return precoOrignal.divide(new BigDecimal("2.0"));
+	}
+
+}
